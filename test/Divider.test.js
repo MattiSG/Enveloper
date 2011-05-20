eb059784f391dddd29efdeb547884a50b5fcf1c8
Matti Schneider-Ghibaudo <alcmene.gs@gmail.com>
2011-05-20 12:22:21 +0200

    Documented blocks. Corrected block init. Added block getter.

--- test/Divider.test.js
+++ test/Divider.test.js
@@ -36,16 +36,13 @@ describe('Divider class', {
 	},
 	
 	"Blocks calculation": function() {
-		points.shift();
-		value_of(subject.initBlocks(points)).should_be([0, 3]);
-		points.unshift({});
-		value_of(subject.initBlocks(points)).should_be([0, 3, 4]);
-		points.unshift({});
 		value_of(subject.initBlocks(points)).should_be([0, 3, 5]);
 		points.unshift({});
 		value_of(subject.initBlocks(points)).should_be([0, 3, 6]);
 		points.unshift({});
 		value_of(subject.initBlocks(points)).should_be([0, 3, 6, 7]);
+		points.unshift({});
+		value_of(subject.initBlocks(points)).should_be([0, 3, 6, 8]);
 	},
 	
 	"Highest point calculation": function() {
@@ -83,6 +80,20 @@ describe('Divider class', {
 												{x: 100, y: 5}
 											])
 				).should_be({x: 1, y: 10});
+	},
+	
+	"Block getting": function() {
+		subject = new Divider(points);
+		
+		value_of(subject.getBlock(0)).should_be(sortedPoints.slice(0, 3));
+		value_of(subject.getBlock(1)).should_be(sortedPoints.slice(3));
+		value_of(subject.getBlock(2)).should_be([]);
+		
+		points.splice(-2);
+		subject = new Divider(points);
+		value_of(subject.getBlock(0)).should_be(sortedPoints.slice(0, 3));
+		value_of(subject.getBlock(1)).should_be([]);
+		value_of(subject.getBlock(2)).should_be([]);
 	}
 });
 
